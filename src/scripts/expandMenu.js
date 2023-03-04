6aa81a1069da3236d0e82373883c8219c5da0922
Eliot Bas <eliot.bas@orange.fr>
2023-03-04 19:25:52 +0100

    add corrections

--- src/scripts/expandMenu.js
+++ src/scripts/expandMenu.js
@@ -4,11 +4,13 @@ const hamburger = document.querySelector('.nav-hamburger');
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('open');
   hamburger.classList.toggle('open');
+  hamburger.ariaLabel = hamburger.classList.contains('open') ? 'Close menu' : 'Open menu';
 });
 
 links.forEach(link => {
   link.addEventListener('click', () => {
     nav.classList.remove('open');
     hamburger.classList.remove('open');
+    hamburger.ariaLabel = 'Open menu';
   });
 });
