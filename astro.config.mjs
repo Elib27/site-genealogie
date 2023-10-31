87f05400ceb423b7031a54c5918bf16c1fd47ae3
Eliot Bas <eliot.bas@orange.fr>
2023-10-31 19:44:39 +0100

    update Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.estienne-genealogie.fr',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), compress()]
 });
